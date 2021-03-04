f0ada0b1de256a9edf8014bfa596ad330633d68c
Cezin <cezin.cupii@extendstudio.com>
2021-03-04 16:27:16 +0200

    Cleaned code

--- Seminar2_CTS/src/ro/ase/cts/program/Program.java
+++ Seminar2_CTS/src/ro/ase/cts/program/Program.java
@@ -2,17 +2,18 @@ package ro.ase.cts.program;
 
 import java.io.FileNotFoundException;
 import ro.ase.cts.clase.Angajat;
-import ro.ase.cts.clase.Utils;
+import ro.ase.cts.clase.Aplicant;
+import ro.ase.cts.clase.readers.AngajatReader;
+
 import java.util.List;
 
 public class Program {
 
 	public static void main(String[] args) {
-		List<Angajat> listaAngajati;
+		List<Aplicant> listaAngajati;
 		try {
-			
-			listaAngajati = Utils.readAngajati("angajati.txt");
-			for(Angajat angajat:listaAngajati)
+			listaAngajati = AngajatReader.readAngajati("angajati.txt");
+			for(Aplicant angajat:listaAngajati)
 				System.out.println(angajat.toString());
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
