cb96ca926acdb0ce8eeccd878c585d059d94d3b5
Cezin Cupii <ccupii@deloittece.com>
2021-03-18 09:16:50 +0200

    Initial commit

---
+++ Seminar4_CTS/src/ro/ase/cts/ro/ase/cts/classes/Vaccin.java
@@ -0,0 +1,48 @@
+package ro.ase.cts.ro.ase.cts.classes;
+
+public class Vaccin {
+    private String numeProducator;
+    private int numarDoze;
+    private float pret;
+
+    private static Vaccin instanta=null;
+
+    public static synchronized Vaccin getInstance(String numeProducator, int numarDoze, float pret) {
+        instanta=(instanta==null ? new Vaccin(numeProducator,numarDoze,pret) : instanta );
+        return instanta;
+    }
+
+
+    public void setNumeProducator(String numeProducator) {
+        this.numeProducator = numeProducator;
+    }
+
+    public void setNumarDoze(int numarDoze) {
+        this.numarDoze = numarDoze;
+    }
+
+    public void setpret(int pret) {
+        this.pret = pret;
+    }
+
+    public static void setInstanta(Vaccin instanta) {
+        Vaccin.instanta = instanta;
+    }
+
+    private Vaccin(String numeProducator, int numarDoze, float pret) {
+        this.numeProducator = numeProducator;
+        this.numarDoze = numarDoze;
+        this.pret = pret;
+    }
+
+    @Override
+    public String toString() {
+        return "Vaccin{" +
+                "numeProducator='" + numeProducator + '\'' +
+                ", numarDoze=" + numarDoze +
+                ", pret=" + pret +
+                '}';
+    }
+}
+
+
--- Seminar4_CTS/src/ro/ase/cts/ro/ase/cts/main/Main.java
+++ Seminar4_CTS/src/ro/ase/cts/ro/ase/cts/main/Main.java
@@ -2,6 +2,7 @@ package ro.ase.cts.ro.ase.cts.main;
 
 import ro.ase.cts.ro.ase.cts.classes.ClinicaVeterinaraLazy;
 import ro.ase.cts.ro.ase.cts.classes.ClinicaVeterniara;
+import ro.ase.cts.ro.ase.cts.classes.Vaccin;
 
 public class Main {
 
@@ -12,17 +13,27 @@ public class Main {
 //        System.out.println(clinica1.toString());
 //        System.out.println(clinica2.toString());
 
-        ClinicaVeterinaraLazy cl1=ClinicaVeterinaraLazy.getInstance("OkMed","Str Poli",4,300);
-        ClinicaVeterinaraLazy cl2=ClinicaVeterinaraLazy.getInstance("LogiMed","Str buzesti",3,500);
+//        ClinicaVeterinaraLazy cl1=ClinicaVeterinaraLazy.getInstance("OkMed","Str Poli",4,300);
+//        ClinicaVeterinaraLazy cl2=ClinicaVeterinaraLazy.getInstance("LogiMed","Str buzesti",3,500);
+//
+//        System.out.println(cl1);
+//        System.out.println(cl2);
+//
+//        cl1.setNume("Clinica 1");
+//        cl2.setNumeMedici(45);
+//
+//        System.out.println(cl1);
+//        System.out.println(cl2);
 
-        System.out.println(cl1);
-        System.out.println(cl2);
 
-        cl1.setNume("Clinica 1");
-        cl2.setNumeMedici(45);
+        Vaccin v1=Vaccin.getInstance("Pfizer",2,225);
 
-        System.out.println(cl1);
-        System.out.println(cl2);
+        System.out.println(v1);
+
+        v1.setpret(555);
+
+        System.out.println(v1);
+
 
     }
 }
